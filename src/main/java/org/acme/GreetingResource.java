acea6a9ee205f4ad75c8be1a9a07919ac154c006
Helber Belmiro <helber.belmiro@gmail.com>
2022-02-25 16:24:06 -0300

    Added Application class
    Signed-off-by: Helber Belmiro <helber.belmiro@gmail.com>

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -1,16 +1,11 @@
 package org.acme;
 
-import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
-import org.eclipse.microprofile.openapi.annotations.info.Info;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("/hello")
-@OpenAPIDefinition(
-        info = @Info(title = "This is the title", description = "This is the API description", version = "v42"))
 public class GreetingResource {
 
     @GET
